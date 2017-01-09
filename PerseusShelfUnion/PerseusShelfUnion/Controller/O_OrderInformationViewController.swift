2f58797c3344c31c4f432b6b580b9c390169a445
BoomHA <13815486558@163.com>
2017-01-09 08:50:56 +0800

    哈哈哈

--- PerseusShelfUnion/PerseusShelfUnion/Controller/O_OrderInformationViewController.swift
+++ PerseusShelfUnion/PerseusShelfUnion/Controller/O_OrderInformationViewController.swift
@@ -56,14 +56,5 @@ class O_OrderInformationViewController: UIViewController {
 
     @IBAction func back(segue:UIStoryboardSegue) {
     }
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
